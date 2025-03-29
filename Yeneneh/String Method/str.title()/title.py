55d14f7ac978beed1b4d15e97a8b34335b40033e
yeneneh7 <yenenehtesfaab805@gmail.com>
2025-03-29 16:10:25 +0300

    Create title.py

---
+++ Yeneneh/String Method/str.title()/title.py
@@ -0,0 +1,8 @@
+
+sentence = "yeneneh is learning to do python"
+
+# Convert to title case
+title_case_sentence = sentence.title()
+
+# Print the result
+print(title_case_sentence)  # Output: "Yeneneh Is Learning To Do Python"
